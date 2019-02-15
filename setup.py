e76f900ea930577d391aad805436884eb00c432a
poppyschmo <poppyschmo@users.noreply.github.com>
2019-02-15 02:02:30 -0800

    Use README as long description in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
+from pathlib import Path
 
 setup(
     name="pytest-pdb-break",
     version="0.0.1",
     license="Apache 2.0",
     description="A pytest command-line option for pdb breakpoints",
+    long_description=Path(__file__).parent.joinpath("README.rst").read_text(),
     url="https://github.com/poppyschmo/pytest-pdb-break",
     author="Jane Soko",
     author_email="poppyschmo@protonmail.com",
