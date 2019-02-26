7252cbae2d06c506c98f86f22e5e1faf840abfec
poppyschmo <poppyschmo@users.noreply.github.com>
2019-02-25 23:07:23 -0800

    Tests: add test for get_collected helper

---
+++ tests/helpers/test_get_collected.py
@@ -0,0 +1,114 @@
+import pytest
+from helpers import get_collected
+from pprint import pformat
+
+f1 = """
+    import pytest
+
+    def test_a():
+        assert True
+
+    @pytest.mark.parametrize('arg', [True, False])
+    def test_b(arg):
+        assert arg
+"""
+
+f2 = """
+    import pytest
+
+    class TestC:
+        def test_c(self):
+            assert True
+
+        @pytest.mark.parametrize('arg', [True, False])
+        def test_d(self, arg):
+            assert arg
+
+        @pytest.fixture
+        def myfix(self):
+            return True
+
+        def test_e(self, myfix):
+            return myfix
+"""
+
+f3 = """
+    import unittest
+    class TestU(unittest.TestCase):
+        def test_x(self):
+            self.assertTrue(True)
+"""
+
+
+@pytest.fixture
+def repo(testdir):
+    testdir.makepyfile(test_fun=f1)
+    testdir.makepyfile(test_cls=f2)
+    testdir.makepyfile(test_ut=f3)
+    return testdir
+
+
+def test_get_items_args(repo):
+    subdir = repo.tmpdir.join("subdir")
+    subdir.mkdir()
+    rv = get_collected._get_items(("-k", "False"))
+    repo.tmpdir.join("got.out").write(pformat(rv))
+    assert repr(rv) == "[<Function test_d[False]>, <Function test_b[False]>]"
+
+
+def test_get_node_ids(repo):
+    rv = get_collected.get_node_ids()
+    repo.tmpdir.join("got.out").write(pformat(rv))
+    assert rv == ["test_cls.py::TestC::test_c",
+                  "test_cls.py::TestC::test_d[True]",
+                  "test_cls.py::TestC::test_d[False]",
+                  "test_cls.py::TestC::test_e",
+                  "test_fun.py::test_a",
+                  "test_fun.py::test_b[True]",
+                  "test_fun.py::test_b[False]",
+                  "test_ut.py::TestU::test_x"]
+
+
+def test_get_locations(repo):
+    rv = get_collected.get_locations()
+    repo.tmpdir.join("got.out").write(pformat(rv))
+    assert rv == [("test_cls.py", 4, "TestC.test_c"),
+                  ("test_cls.py", 7, "TestC.test_d"),
+                  ("test_cls.py", 15, "TestC.test_e"),
+                  ("test_fun.py", 3, "test_a"),
+                  ("test_fun.py", 6, "test_b"),
+                  ("test_ut.py", 3, "TestU.test_x")]
+
+
+def test_get_collected(repo):
+    rv = get_collected.get_collected()
+    repo.tmpdir.join("got.out").write(pformat(rv))
+    pfun = repo.tmpdir.join("test_fun.py").strpath
+    pcls = repo.tmpdir.join("test_cls.py").strpath
+    put = repo.tmpdir.join("test_ut.py").strpath
+    assert rv == [
+        {'file': pcls, 'lnum': 4, 'name': 'TestC.test_c',
+         'class_name': 'TestC', 'func_name': 'test_c',
+         'param_id': None, 'nodeid': 'test_cls.py::TestC::test_c'},
+        {'file': pcls, 'lnum': 7, 'name': 'TestC.test_d[True]',
+         'class_name': 'TestC', 'func_name': 'test_d',
+         'param_id': 'True', 'nodeid': 'test_cls.py::TestC::test_d[True]'},
+        {'file': pcls, 'lnum': 7, 'name': 'TestC.test_d[False]',
+         'class_name': 'TestC', 'func_name': 'test_d',
+         'param_id': 'False', 'nodeid': 'test_cls.py::TestC::test_d[False]'},
+        {'file': pcls, 'lnum': 15, 'name': 'TestC.test_e',
+         'class_name': 'TestC', 'func_name': 'test_e',
+         'param_id': None, 'nodeid': 'test_cls.py::TestC::test_e'},
+        {'file': pfun, 'lnum': 3, 'name': 'test_a',
+         'class_name': None, 'func_name': 'test_a',
+         'param_id': None, 'nodeid': 'test_fun.py::test_a'},
+        {'file': pfun, 'lnum': 6, 'name': 'test_b[True]',
+         'class_name': None, 'func_name': 'test_b',
+         'param_id': 'True', 'nodeid': 'test_fun.py::test_b[True]'},
+        {'file': pfun, 'lnum': 6, 'name': 'test_b[False]',
+         'class_name': None, 'func_name': 'test_b',
+         'param_id': 'False', 'nodeid': 'test_fun.py::test_b[False]'},
+        {'file': put, 'lnum': 3, 'name': 'TestU.test_x',
+         'class_name': 'TestU', 'func_name': 'test_x',
+         'param_id': None, 'nodeid': 'test_ut.py::TestU::test_x'}
+    ]
