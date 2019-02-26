645de2a5ac4c3198dc6fb540bca5aec9737aa606
poppyschmo <poppyschmo@users.noreply.github.com>
2019-02-25 23:07:23 -0800

    Emacs: mention license in library header

--- emacs/pytest-pdb-break.el
+++ emacs/pytest-pdb-break.el
@@ -8,15 +8,21 @@
 ;; Keywords: languages, tools
 ;; Package-Requires: ((emacs "25"))
 
-;; This file is not part of GNU Emacs.
+;; This file is not part of GNU Emacs and is licensed under Apache 2.0:
+;; http://www.apache.org/licenses/LICENSE-2.0
 
 ;;; Commentary:
 
 ;; Usage: with point in the body of some test, run M-x `pytest-pdb-break-here'
 ;;
 ;; This command can only handle the "console script"/entry-point invocation
-;; style (as opposed to "python -m pytest").  If a pytest executable doesn't
-;; appear in PATH, `pytest-pdb-break-pytest-executable' must be set.
+;; style (as opposed to "python -m pytest").  If a pytest executable isn't
+;; visible in the environment prepared by the various python-shell-calculate-
+;; functions, `pytest-pdb-break-pytest-executable' must be set.
+;;
+;; Note: at present, the pytest plugin requires Python 3.6+, which means so
+;; does the main command.  However, the minor mode along with the helpers in
+;; the "extras" file should support 3.5.
 ;;
 ;; TODO:
 ;; - Fixture breaking
